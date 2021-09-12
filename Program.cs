aa6c011230960058b654c5a3885d5d35caf71323
HenriqueMiossi <henriquemiossi@gmail.com>
2021-09-12 15:02:16 -0300

    Add initial project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Loja_de_Roupas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
