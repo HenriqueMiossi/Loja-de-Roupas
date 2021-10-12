eb7a36f4eafc77631c2758b3c43a7255722cda91
HenriqueMiossi <henriquemiossi@gmail.com>
2021-10-12 10:57:40 -0300

    Style: fix missing semicolons, fix constructor parameter naming
    Obs: now the Produtos attribute was changed to produtos, as the best way to
    represent a List we found was not using a prop for it.

--- src/Venda.cs
+++ src/Venda.cs
@@ -1,24 +1,27 @@
+using System;
+using System.Collections.Generic;
+
 class Venda
 {
-	public int Id
-	public double Valor
-	public IFormaPagamento FormaPagamento
-	public List Produtos
-	public Cliente Cliente
-	public Vendedor Vendedor
+	List<Produto> _produtos = new List<Produto>();
+	public int Id;
+	public double Valor;
+	public IFormaPagamento FormaPagamento;
+	public Cliente Cliente;
+	public Vendedor Vendedor;
 	
-	public Venda(int Id, double Valor, IFormaPagamento FormaPagamento, Produtos Produtos, Cliente Cliente, Vendedor Vendedor)
+	public Venda(int id, double valor, IFormaPagamento formaPagamento, List<Produto> produtos, Cliente cliente, Vendedor vendedor)
 	{
 		Id = id;
 		Valor = valor;
 		FormaPagamento = formaPagamento;
-		Produtos = produto;
+		_produtos = produtos;
 		Cliente = cliente;
 		Vendedor = vendedor;
 	}
 
 	public override string ToString()
 	{
-		return "Id: " + Id + "\nValor: " + Valor + "\nFormaPagamento: " + FormaPagamento + "\nProdutos: " + Produtos + "\nCliente: " + Cliente + "\nVendedor: " + Vendedor; 
+		return "Id: " + Id + "\nValor: " + Valor + "\nFormaPagamento: " + FormaPagamento + "\nProdutos: " + _produtos + "\nCliente: " + Cliente + "\nVendedor: " + Vendedor; 
 	}
 }
