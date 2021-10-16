ce5dcfd50c208701425b606e9bf7786d831b5834
HenriqueMiossi <henriquemiossi@gmail.com>
2021-10-16 12:40:42 -0300

    Feat: Cliente and Vendedor inherit from Pessoa, create getNome() and ToString() methods

--- src/Cliente.cs
+++ src/Cliente.cs
@@ -1,8 +1,17 @@
 using System.Collections.Generic;
 
-class Cliente
+class Cliente : Pessoa
 {
     List<Venda> _historicoCompras = new List<Venda>();
+
+    public Cliente(string nome, string cpf, string email, Endereco endereco, int telefone)
+    {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
 
     public List<Venda> getHistoricoCompras()
     {
@@ -12,5 +21,15 @@ class Cliente
     public void addCompra(Venda novaVenda)
     {
         _historicoCompras.Add(novaVenda);
+    }
+
+    public override string ToString()
+    {
+        return "Nome: " + nome + "\nCPF: " + cpf + "\nEmail: " + email + "\nEndereço: " + endereco + "\nTelefone: " + telefone;
+    }
+
+    public string getNome()
+    {
+        return nome;
     }
 }
--- src/Vendedor.cs
+++ src/Vendedor.cs
@@ -1,16 +1,27 @@
-class Vendedor
+class Vendedor : Pessoa
 {
     public double Salario { get; set; }
     public int Matricula { get; set; }
 
-    public Vendedor(double salario, int matricula)
+    public Vendedor(double salario, int matricula, string nome, string cpf, string email, Endereco endereco, int telefone)
     {
         Salario = salario;
         Matricula = matricula;
+
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
     }
 
     public override string ToString()
-	{
-		return "Salario" + Salario + "\n Matricula" + Matricula; 
-	}
+    {
+        return "Salario: " + Salario + "\n Matricula: " + Matricula;
+    }
+
+    public string getNome()
+    {
+        return nome;
+    }
 }
